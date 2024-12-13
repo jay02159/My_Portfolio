eac455de089e0acf9ffd972bd0d5281c366162f3
jay jaswanth <58889998+jay02159@users.noreply.github.com>
2024-12-13 05:57:29 +0530

    Update skills.ts

--- my_portfolio/src/constants/skills.ts
+++ my_portfolio/src/constants/skills.ts
@@ -1,1 +1,1 @@
-export const SKILLS = ['UI/UX', 'React', 'TypeScript', 'Figma', 'Node.js'];
+export const SKILLS = ['UI/UX', 'React', 'TypeScript', 'Figma', 'Node.js', 'Prototyping', 'AI Tools', 'Blender', 'IoT Automation & Integration' ];
